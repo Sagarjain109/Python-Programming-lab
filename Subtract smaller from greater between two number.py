b848327f945b1b0c7ef9458e626e5eca3db79889
Sagarjain109 <92741997+Sagarjain109@users.noreply.github.com>
2022-05-27 02:00:52 +0530

    Program to input two numbers and subtract the smaller number from the greater number

---
+++ Subtract smaller from greater between two number.py
@@ -0,0 +1,9 @@
+x=eval(input("Enter first number: "))
+y=eval(input("Enter second number: "))
+differ=0
+if x>y:
+    differ=x-y
+    print("Difference is",differ)
+else:
+    differ=y-x
+    print("Difference is",differ)
